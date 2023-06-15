48294e60d24fe64173a51bc1851240ab5aad01b0
Emiliano Sinatra <58446200+emisinatra@users.noreply.github.com>
2023-06-15 17:17:43 -0300

    install and config chakra ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,26 @@
 import "@/styles/globals.css";
 import "react-toastify/dist/ReactToastify.css";
+import { ChakraProvider } from "@chakra-ui/react";
 import { ToastContainer } from "react-toastify";
-
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Component {...pageProps} />
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-      />
+      <ChakraProvider>
+        <Component {...pageProps} />
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
+      </ChakraProvider>
     </>
   );
 }
