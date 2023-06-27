7a595864403484a8f762f1b5cf1df420272feae8
lucianomasuelli <lucianomasuelli.lm@gmail.com>
2023-06-27 20:04:08 -0300

    Second image added to main body

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -6,23 +6,40 @@ const Body = () => {
     <Stack>
       <Image
         src="/diseño01.png"
-        boxSize={350}
+        boxSize={375}
         objectFit={"cover"}
         marginLeft={"-24"}
+        marginBottom={"-44"}
       />
-      <Stack paddingX={"20"}>
-        <Text textColor={"white"} fontWeight={"bold"} fontSize={"8xl"}>
-          CREATING BONDS
-        </Text>
-        <Stack direction={"row"} fontWeight={"bold"} fontSize={"8xl"}>
-          <Text textColor={"white"} >
-            JOINING PEOPLE
+      <Stack paddingX={"20"} direction={"row-reverse"} justifyContent={"space-between"}>
+      <Image
+          src="/diseño02.png"
+          boxSize={1000}
+          objectFit={"cover"}
+          marginTop={"-80"}
+          marginRight={"-96"}
+        />
+        <Stack marginRight={"-20"}>
+          <Text textColor={"white"} fontWeight={"bold"} fontSize={"8xl"}>
+            CREATING BONDS
           </Text>
-          <Text textColor={"primary.500"}>.</Text>
+          <Stack direction={"row"} fontWeight={"bold"} fontSize={"8xl"}>
+            <Text textColor={"white"}>JOINING PEOPLE</Text>
+            <Text textColor={"primary.500"}>.</Text>
+          </Stack>
+          <Stack maxW={860} textAlign={"left"}>
+            <Text textColor={"white"} fontWeight={"300"} fontSize={"20"}>
+              es una agencia de creatividad y diseño situada en Córdoba,
+              Argentina.Hace más de diez años trabajamos transversalmente con
+              clientes de todo tipo. Acompañando desde compañías internacionales
+              hasta startups locales, para ayudarlas a conectarse con las
+              personas. Porque de eso se trata. De que las marcas que pasan por
+              nuestra agencia logren repercutir en un mundo totalmente
+              cambiante.
+            </Text>
+          </Stack>
         </Stack>
-        <Stack maxW={860} textAlign={"left"}>
-          <Text textColor={"white"} fontWeight={"300"} fontSize={"20"} >es una agencia de creatividad y diseño situada en Córdoba, Argentina.Hace más de diez años trabajamos transversalmente con clientes de todo tipo. Acompañando desde compañías internacionales hasta startups locales, para ayudarlas a conectarse con las personas. Porque de eso se trata. De que las marcas que pasan por nuestra agencia logren repercutir en un mundo totalmente cambiante.</Text>  
-        </Stack>
+        
       </Stack>
     </Stack>
   );
