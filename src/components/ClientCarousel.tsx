aac54f9352257dbeeef81d22205af033960c34e5
Andres Maglione <amaglione2002@gmail.com>
2023-07-10 18:10:45 -0300

    Add remaining clients

--- src/components/ClientCarousel.tsx
+++ src/components/ClientCarousel.tsx
@@ -7,34 +7,12 @@ import Marquee from "react-fast-marquee";
 const clients = [
   { name: "ibox", pathToLogo: "/clients/IBOX REDUCIDO.png" },
   { name: "monki", pathToLogo: "/clients/MONKI REDUCIDO.png" },
+  { name: "adarvez", pathToLogo: "/clients/adarvez.png" },
+  { name: "casa vera", pathToLogo: "/clients/casa vera.png" },
+  { name: "quinoa", pathToLogo: "/clients/quinoa.png" },
 ];
 
 export const ClientCarousel = () => {
-  const settings = {
-    infinite: true,
-    slidesToShow: 2,
-    slidesToScroll: 1,
-    autoplay: true,
-    speed: 3000,
-    autoplaySpeed: 3000,
-    cssEase: "linear",
-    arrows: false,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-        },
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 1,
-        },
-      },
-    ],
-  };
-
   return (
     <Box
       marginX={10}
